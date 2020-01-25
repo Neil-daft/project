49d1eae2c0e12c81d754377c71188ea161d62218
Neil McMahon <neil.mcmahon@distilled.ie>
2020-01-25 11:09:10 +0000

    update flex

---
+++ src/Domain/Status.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Domain;
+
+class Status
+{
+    const STATUS_PENDING = 'pending';
+    const STATUS_ACTIVE = 'active';
+}
---
+++ src/Service/ProjectService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Service;
+
+use App\Domain\Status;
+use App\Entity\Project;
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProjectService
+{
+    /** @var ProjectRepository */
+    private $projectRepository;
+
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    public function __construct(ProjectRepository $projectRepository, EntityManagerInterface $entityManager)
+    {
+        $this->projectRepository = $projectRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @return Project[]
+     */
+    public function getProjectsOrderedByDate()
+    {
+        return $this->projectRepository->findBy([], ['createdAt' => 'desc']);
+    }
+
+    public function save(Project $project): void
+    {
+        $this->entityManager->persist($project);
+        $this->update();
+    }
+
+    public function delete(Project $project): void
+    {
+        $this->entityManager->remove($project);
+        $this->update();
+    }
+
+    public function update(): void
+    {
+        $this->entityManager->flush();
+    }
+
+    public function approve(Project $project)
+    {
+        $project->setStatus(Status::STATUS_ACTIVE);
+        $this->update();
+    }
+
+}
